a827b0cd1618087990f4cd382596cf050a60b5f7
David <122915174+davidehibello@users.noreply.github.com>
2026-04-18 13:32:55 -0400

    Delete Nested List Parser API project directory

--- Nested List Parser API project/src/ListComponent.java
+++
@@ -1,7 +0,0 @@
-public interface ListComponent {
-    void printValue();
-    void addChild(int index, ListComponent child);
-    void removeChild(int index);
-    ListComponent getChild(int index);
-    int getChildCount();
-}
